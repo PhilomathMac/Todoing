627d124296b78eea99752af9e9bf438dce2de45b
KenzieMac <kenziemacd@icloud.com>
2022-09-02 13:29:58 -0700

    - Add item model

---
+++ Todoing/Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoing
+//
+//  Created by McKenzie Macdonald on 9/2/22.
+//
+
+import Foundation
+
+struct Item {
+    var title: String = ""
+    var done: Bool = false
+}
