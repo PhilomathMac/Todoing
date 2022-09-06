1b93ab90d45a8a1248804cfea6efbcaa5c12f3ea
KenzieMac <kenziemacd@icloud.com>
2022-09-06 14:26:29 -0700

    Implement loading and saving lists into coreData

--- Todoing/ViewControllers/ListsTableViewController.swift
+++ Todoing/ViewControllers/ListsTableViewController.swift
@@ -6,26 +6,68 @@
 //
 
 import UIKit
+import CoreData
 
 class ListsTableViewController: UITableViewController {
+    
+    var listsArray = [List]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadItems()
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        
     }
-    
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
+}
+
+// MARK: - TableView DataSource
+
+extension ListsTableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return listsArray.count
     }
+}
 
+// MARK: - TableView Delegate
+
+extension ListsTableViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+    }
+    
+}
+
+// MARK: - Data Manipulation Methods
+extension ListsTableViewController {
+    
+    func addItems() {
+       
+        do {
+            try context.save()
+        } catch {
+            print("Error saving list: \(error.localizedDescription)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func loadItems(with request: NSFetchRequest<List> = List.fetchRequest()) {
+        do {
+            listsArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data: \(error.localizedDescription)")
+        }
+        
+        tableView.reloadData()
+    }
+    
 }
