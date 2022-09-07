ddf56675f4f1e95e44ece52b645cb9db8366ca91
McKenzie Macdonald <kenziemacd@icloud.com>
2022-09-07 11:47:07 -0700

    Add realm packages to app

--- Todoing/AppDelegate.swift
+++ Todoing/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-                
+        
+        // Create new realm (like a persistent container)
+        do {
+            let realm = try Realm()
+        } catch {
+            print("Error creating realm: \(error.localizedDescription)")
+        }
         return true
     }
 
